eab550aabaa26be640ddbedfa98c583e34825d6c
sgalyeon <sgalyeon@student.21-school.ru>
2025-01-23 08:56:02 +0300

    add_auth

--- vlad_project/vlad_site/views.py
+++ vlad_project/vlad_site/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -29,8 +31,25 @@ def rules_of_use(request):
     return render(request, 'rules.html')
 
 
-def login_view(request):
-    return render(request, 'login.html')
+def login_view(request, user):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('')
+    else:
+
+        return redirect('login')
+
 
 def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            login_view(request, user)
     return render(request, 'register.html')
